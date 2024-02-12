9ed4621ddd8470358c7a8c9c89023c5e287f4a6e
skaimarvas <skaimarvas@gmail.com>
2024-02-12 15:35:37 +0300

    project was created

---
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/S18d1jpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.s18d1jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18d1jpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18d1jpaApplication.class, args);
+	}
+
+}
