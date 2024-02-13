4a2b3866057a469e5d0c7c980d30aa6f36725f16
skaimarvas <skaimarvas@gmail.com>
2024-02-13 14:22:03 +0300

    fields were added in Burger class

--- s18d1jpa/src/main/java/com/example/s18d1jpa/entity/Burger.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/entity/Burger.java
@@ -1,4 +1,13 @@
 package com.example.s18d1jpa.entity;
 
+
+/**Notes:
+ *
+ */
 public class Burger {
+  private int id;
+  private String name;
+  private boolean isVegan;
+  private BreadType breadType;
+  private String contents;
 }
