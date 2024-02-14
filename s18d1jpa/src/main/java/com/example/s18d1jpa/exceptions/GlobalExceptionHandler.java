c700bbbd3ac11ed34188e1c18770698067020eb0
skaimarvas <skaimarvas@gmail.com>
2024-02-14 14:46:19 +0300

    @Slf4j and @ControllerAdvice annotations were added

--- s18d1jpa/src/main/java/com/example/s18d1jpa/exceptions/GlobalExceptionHandler.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,9 @@
 package com.example.s18d1jpa.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
 public class GlobalExceptionHandler {
 }
