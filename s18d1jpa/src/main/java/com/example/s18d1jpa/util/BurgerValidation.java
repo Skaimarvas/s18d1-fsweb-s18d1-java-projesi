dc877658e1bec31733e68b4fd4cfa8ab60792e4d
skaimarvas <skaimarvas@gmail.com>
2024-02-14 14:44:18 +0300

    static checkname method was added

--- s18d1jpa/src/main/java/com/example/s18d1jpa/util/BurgerValidation.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/util/BurgerValidation.java
@@ -1,5 +1,13 @@
 package com.example.s18d1jpa.util;
 
-public class BurgerValidation {
+import com.example.s18d1jpa.exceptions.BurgerException;
+import org.springframework.http.HttpStatus;
 
+public class BurgerValidation {
+  public static void checkname(String name){
+      if(name == null || name.isEmpty()){
+          throw new BurgerException("Name is null or empty!", HttpStatus.BAD_REQUEST);
+
+      }
+  }
 }
