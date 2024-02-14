c153c84aab426cb8fd34f57e328ce3ad36bedc7e
skaimarvas <skaimarvas@gmail.com>
2024-02-14 15:09:08 +0300

    BurgerController constructor was created via Autowired annotation

---
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/controller/BurgerController.java
@@ -0,0 +1,19 @@
+package com.example.s18d1jpa.controller;
+
+
+import com.example.s18d1jpa.dao.BurgerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/burger")
+public class BurgerController {
+    private final BurgerDao burgerDao;
+    @Autowired
+    public BurgerController(BurgerDao burgerDao){
+        this.burgerDao = burgerDao;
+    }
+}
