40793a24be87f44d9e6dffe2a4686bfaaf68cd73
skaimarvas <skaimarvas@gmail.com>
2024-02-14 13:48:10 +0300

    all implement methods were added on BurgerDaoImpl

--- s18d1jpa/src/main/java/com/example/s18d1jpa/dao/BurgerDaoImpl.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/dao/BurgerDaoImpl.java
@@ -1,4 +1,49 @@
 package com.example.s18d1jpa.dao;
 
-public class BurgerDaoImpl {
+import com.example.s18d1jpa.entity.BreadType;
+import com.example.s18d1jpa.entity.Burger;
+
+import java.util.List;
+
+public class BurgerDaoImpl implements BurgerDao {
+
+    @Override
+    public Burger save(Burger burger) {
+        return null;
+    }
+
+    @Override
+    public Burger findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Burger update(Burger newburger) {
+        return null;
+    }
+
+    @Override
+    public Burger remove(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Burger> findByPrice(int price) {
+        return null;
+    }
+
+    @Override
+    public List<Burger> findByBreadType(BreadType breadType) {
+        return null;
+    }
+
+    @Override
+    public List<Burger> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Burger> findByContent(String content) {
+        return null;
+    }
 }
