de74f13e164b72b2aba27e34e6606462b8a18547
skaimarvas <skaimarvas@gmail.com>
2024-02-14 13:44:42 +0300

    all methods were created

--- s18d1jpa/src/main/java/com/example/s18d1jpa/dao/BurgerDao.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/dao/BurgerDao.java
@@ -1,4 +1,17 @@
 package com.example.s18d1jpa.dao;
 
+import com.example.s18d1jpa.entity.BreadType;
+import com.example.s18d1jpa.entity.Burger;
+
+import java.util.List;
+
 public interface BurgerDao {
+    Burger save(Burger burger);
+    Burger findById(int id);
+    Burger update(Burger newburger);
+    Burger remove(int id);
+    List<Burger> findByPrice(int price);
+    List<Burger> findByBreadType(BreadType breadType);
+    List<Burger> findAll();
+    List<Burger> findByContent(String content);
 }
