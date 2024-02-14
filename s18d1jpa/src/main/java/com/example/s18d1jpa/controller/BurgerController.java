cbc370a046aac1168013e3bf5f94594924fc2ef0
skaimarvas <skaimarvas@gmail.com>
2024-02-14 15:41:18 +0300

    findAll() method was added

--- s18d1jpa/src/main/java/com/example/s18d1jpa/controller/BurgerController.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/controller/BurgerController.java
@@ -7,6 +7,8 @@ import com.example.s18d1jpa.util.BurgerValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/burger")
@@ -22,6 +24,9 @@ public class BurgerController {
         BurgerValidation.checkname(burger.getName());
         return burgerDao.save(burger);
     }
-
+    @GetMapping
+    public List<Burger> findAll(){
+        return burgerDao.findAll();
+    }
 
 }
