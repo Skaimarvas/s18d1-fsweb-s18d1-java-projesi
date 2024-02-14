73684368e2b3bb3551616167f19216dafbbfabca
skaimarvas <skaimarvas@gmail.com>
2024-02-14 14:35:02 +0300

    constructor and @Getter annotation was added.

--- s18d1jpa/src/main/java/com/example/s18d1jpa/exceptions/BurgerException.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/exceptions/BurgerException.java
@@ -1,4 +1,13 @@
 package com.example.s18d1jpa.exceptions;
 
-public class BurgerException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BurgerException extends RuntimeException {
+    private HttpStatus status;
+    public BurgerException(String message,HttpStatus status){
+        super(message);
+        this.status = status;
+    }
 }
