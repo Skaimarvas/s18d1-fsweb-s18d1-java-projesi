1b03c154f527d4619ad630f74a0875c207da53f6
skaimarvas <skaimarvas@gmail.com>
2024-02-14 14:39:45 +0300

    @Data and @AllArgsConstructor annotations were added and message field added

--- s18d1jpa/src/main/java/com/example/s18d1jpa/exceptions/BurgerErrorResponse.java
+++ s18d1jpa/src/main/java/com/example/s18d1jpa/exceptions/BurgerErrorResponse.java
@@ -1,4 +1,10 @@
 package com.example.s18d1jpa.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class BurgerErrorResponse {
+    private String message;
 }
